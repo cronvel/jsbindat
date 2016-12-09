7ddd1c226bc3a1d5e6c47d6ffa3c1ecf80500a3b
Cédric Ronvel <cedric.ronvel@gmail.com>
2016-12-09 21:59:41 +0100

    wip: bugfixing real-world test

--- sample/test.js
+++ sample/test.js
@@ -2,11 +2,20 @@
 var fs = require( 'fs' ) ;
 var jsbindat = require( '../lib/jsbindat.js' ) ;
 
-var data = require( './sample1.json' ) ;
+//var data = require( './sample1.json' ) ;
+var data = require( './sample2.js' ) ;
 var stream = fs.createWriteStream( __dirname + '/out.jsdat' ) ;
+
+var string = require( 'string-kit' ) ;
 
 jsbindat.serialize( data , stream , {} , function() {
 	stream.end() ;
+	
+	stream = fs.createReadStream( __dirname + '/out.jsdat' ) ;
+	jsbindat.unserialize( stream , {} , function( udata ) {
+		
+		console.log( "After serialize/unserialize:\n" + string.inspect( { style: "color" , depth: Infinity } , udata ) ) ;
+	} ) ;
 } ) ;
 
 
